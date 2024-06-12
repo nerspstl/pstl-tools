77b0c6eb2708b639c6e723f2f882a752cfe19ee7
tyjoto <tyjoto@gmail.com>
2024-06-12 15:07:50 -0400

    bump version v2024.06.0 -> v2024.06.1

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Python scripts for making working with UMICH NERS PSTL Lab Equipment much easier
 
 ## Current Version
-v2024.06.0
+v2024.06.1
 
 ## Tested Python Versions
 - v3.11
--- src/pstl/__init__.py
+++ src/pstl/__init__.py
@@ -7,7 +7,7 @@ Tools for working with PSTL equipment easier
 
 """
 
-__version__ = 'v2024.06.0'
+__version__ = 'v2024.06.1'
 __author__ = 'umich-pstl'
 
 from pstl import diagnostics
