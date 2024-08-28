842bf0d36d309671a69e2e57319bedf1a1d4a4bf
tyjoto <tyjoto@gmail.com>
2024-08-28 17:09:12 -0400

    defined setup function to build rpa

--- src/pstl/diagnostics/probes/rpa/rpa.py
+++ src/pstl/diagnostics/probes/rpa/rpa.py
@@ -7,9 +7,43 @@ from pstl.utls.objects import setup as object_setup
 from pstl.diagnostics.probes.classes import Probe
 
 available_probe_classes = [
-    ["4grid", "four", "fourgrid"],
-    [ "3grid", "three","threegrid"],
+    [4, "4grid", "four", "fourgrid"],
+    [ 3, "3grid", "three","threegrid"],
 ]
+
+def setup(settings, *args, **kwargs):
+    """
+    Creates and returns a RPA probe object  based on settings dictionary passed in.
+    The settings parameter must have keys 'ngrids'.
+    
+    Keys:
+        'ngrids'     : int   ->  number of grids [4,3]
+    (optional)
+        'name'      : str   ->  name designation for probe 
+        'args'      : tuple ->  addional position arguments
+        'kwargs'    : dict  ->  addional keyword arguments
+
+    Returns: Probe Object
+    """
+    # determine shape of 
+    shape = settings.pop("ngrids").lower()
+    if shape in available_probe_classes[0]:
+        Probe = FourGridRPA
+    elif shape in available_probe_classes[1]:
+        Probe = ThreeGridRPA
+    else:
+        raise ValueError("'%s' is not a valid option."%(shape))
+    
+    output_object: RepellingPotentialAnalyzer = object_setup(
+        *args,
+        settings=settings,
+        builders={},
+        Builder=Probe,
+    #    to_args=
+        **kwargs,
+    )
+
+    return output_object
 
 class RepellingPotentialAnalyzer(Probe, ABC):
     def __init__(self, *args, ngrids: int | None = None, name=None, description=None, **kwargs) -> None:
